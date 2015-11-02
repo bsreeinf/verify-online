b840077d85eef258f09eb95e9dae833ad507b8d1
Sreenath B <bsreeinf@gmail.com>
2015-11-03 03:25:42 +0530

    design tweaks and clean commit

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,13 @@ Rails.application.routes.draw do
   get    'status'   => 'student_verification#status'
   get    'history'  => 'student_verification#history'
 
-
+  
 
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
 
   resources :users
+  resources :colleges
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
