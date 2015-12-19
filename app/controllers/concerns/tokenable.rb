a1ace671895e5e8745376529b50a72d82ff9c2c3
Sreenath B <bsreeinf@gmail.com>
2015-12-19 07:02:14 +0530

    bug mix in tokenable

--- app/controllers/concerns/tokenable.rb
+++ app/controllers/concerns/tokenable.rb
@@ -10,7 +10,7 @@ module Tokenable
   def generate_token
     self.verification_token = loop do
       # random_token = SecureRandom.base64(5).gsub("/","_").gsub(/=+$/,"")
-      random_token = rand(36**length).to_s(36)
+      random_token = rand(36**7).to_s(36)
       break random_token unless self.class.exists?(verification_token: random_token)
     end
   end
