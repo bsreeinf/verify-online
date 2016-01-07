115284044b40fd1a341c1bf08f6cf4429ae61fa6
Allen Davis <harmonyinceltic@gmail.com>
2016-01-07 12:48:52 +0530

    custom verification page in active admin

--- app/admin/verification_request.rb
+++ app/admin/verification_request.rb
@@ -13,5 +13,44 @@ ActiveAdmin.register VerificationRequest do
 #   permitted
 # end
 
+index :title => "Verification Requests" do |verification_request|
+	selectable_column
+       column "Sl. no.", :id
+       column "Verification ID", :verification_token
+       column "Name", :name
+       column "Hallticket no", :hallticket_no
+       column "Requested on", :created_at
 
-end
+
+       column "Document ", :document_link 
+
+       column "Document" do |verification_request|
+		   raw "<a href='#{verification_request.document_link }' target='blank'>download</a>"
+	   end
+
+       column "College" do |verification_request|
+		   verification_request.college.name
+	   end
+	   column "Amount", :amount
+       column "Status" do |verification_request|
+		   verification_request.verification_status.description
+	   end
+
+	   column "Report Date" do |verification_request|
+	   		if verification_request.verification_status_id !=1
+	   			raw "#{local_time(verification_request.updated_at + 330.minutes, '%d/%m/%Y %l:%M%p')}"
+	   		end
+	   end
+
+	   column "Report" do |verification_request|
+		   raw "<a href='/final_report_user.pdf?verification_id=#{verification_request.id}' target='blank'>download</a>"
+	   end
+	   
+        
+
+
+	       
+	  actions
+	end
+
+ end
