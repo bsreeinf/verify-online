1d38c04e3169cee1b15077cf10d1bdada38cba69
Sreenath B <bsreeinf@gmail.com>
2016-02-05 20:01:31 +0530

    report default dat

--- app/controllers/report_data_controller.rb
+++ app/controllers/report_data_controller.rb
@@ -3,6 +3,42 @@ class ReportDataController < InheritedResources::Base
   before_action :set_report_datum, only: [:index, :update]
 
   def index
+    if @report_datum.from_address.nil?
+      @from_address = "Default From Address" 
+    else 
+      @from_address = @report_datum.from_address
+    end
+
+    if @report_datum.letter_title.nil?
+      @letter_title = "Ref : Your letter dated _REQUEST_DATE_ for Verification."
+    else 
+      @letter_title = @report_datum.letter_title
+    end
+
+    if @report_datum.to_address.nil?
+      @to_address = "_USER_TO_ADDRESS_"
+    else 
+      @to_address = @report_datum.to_address
+    end
+
+    if @report_datum.subject.nil?
+      @subject = "Regarding Education Verification."
+    else 
+      @subject = @report_datum.subject
+    end
+
+    if @report_datum.body.nil?
+      @body = "Mollitia suscipit qui consequatur id quo adipisci omnis. Soluta molestiae reprehenderit provident quibusdam dolorem recusandae. Exercitationem culpa aperiam perferendis vitae adipisci nihil sit modi. Atque quia suscipit esse quae dicta vitae. _STATUS_ Autem illum dolorum officiis laborum quis. Deserunt cumque exercitationem id numquam nobis magnam quia voluptatibus. Ea accusamus quia neque dicta est quas. Officiis dicta et aut qui iste in aut."
+    else 
+      @body = @report_datum.body
+    end
+
+    if @report_datum.designation.nil?
+      @designation = "Controller of Examination"
+    else 
+      @designation = @report_datum.designation
+    end
+
     respond_to do |format|
       format.html
       format.json {render json: @report_datum}
