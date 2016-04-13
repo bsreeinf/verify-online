a6811069c6d4c3df7240e60af236974e501fa071
Sreenath B <bsreeinf@gmail.com>
2016-04-13 18:55:56 +0530

    minor report changes

--- app/models/report_datum.rb
+++ app/models/report_datum.rb
@@ -1,14 +1,10 @@
 class ReportDatum < ActiveRecord::Base
   belongs_to :college
 
-  has_attached_file :header
-  has_attached_file :document,
+  has_attached_file :header,
                     :styles => { :medium => ["1000x200>", :jpg]},
                     :processors => [:ghostscript, :thumbnail]
-  has_attached_file :signature,
-                    :styles => { :medium => ["1000x200>", :jpg]},
-                    :processors => [:ghostscript, :thumbnail]
-
+  has_attached_file :signature
   validates_attachment_content_type :header, :content_type => /\Aimage\/.*\Z/
   validates_attachment_content_type :signature, :content_type => /\Aimage\/.*\Z/
 end
