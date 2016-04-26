705a697123f502063a2dd161ca72fc07bec6394c
Sreenath B <bsreeinf@gmail.com>
2016-04-26 17:43:09 +0530

    Payment condition bug fix in controllers

--- app/controllers/college_verification_controller.rb
+++ app/controllers/college_verification_controller.rb
@@ -5,7 +5,7 @@ class CollegeVerificationController < ApplicationController
   def index
   	if params.has_key?(:search_tag)
       @college_verifications =  VerificationRequest.all.where(
-        " payment_id != null AND 
+        " payment_id NOT null AND 
           college_id = ? AND 
           verification_status_id <= ? AND 
           (
@@ -37,7 +37,7 @@ class CollegeVerificationController < ApplicationController
   def completed
     if params.has_key?(:search_tag)
       @college_verifications_completed =  VerificationRequest.all.where(
-        " payment_id != null AND college_id = ? AND verification_status_id > ? AND (hallticket_no ILIKE ? OR name ILIKE ? OR verification_token ILIKE ?)", 
+        " payment_id NOT null AND college_id = ? AND verification_status_id > ? AND (hallticket_no ILIKE ? OR name ILIKE ? OR verification_token ILIKE ?)", 
           @college_id, 
           2, 
           "%#{params[:search_tag]}%",
@@ -47,7 +47,7 @@ class CollegeVerificationController < ApplicationController
       .paginate(page: params[:page],:per_page => 10)
     else  
       @college_verifications_completed =  VerificationRequest.all.where(
-        "college_id = ? AND verification_status_id > ?", 
+        " payment_id NOT null AND college_id = ? AND verification_status_id > ?", 
           @college_id, 
           2
         ).order('created_at DESC')
--- app/controllers/student_verification_controller.rb
+++ app/controllers/student_verification_controller.rb
@@ -70,7 +70,7 @@ class StudentVerificationController < ApplicationController
 	def status
 		if params.has_key?(:search_tag)
 	      @verifications =  VerificationRequest.all.where(
-	        " payment_id != null AND user_id = ? AND (hallticket_no ILIKE ? OR name ILIKE ? OR verification_token ILIKE ?)", 
+	        " payment_id NOT null AND user_id = ? AND (hallticket_no ILIKE ? OR name ILIKE ? OR verification_token ILIKE ?)", 
 	          current_user.id, 
 	          "%#{params[:search_tag]}%",
 	          "%#{params[:search_tag]}%",
