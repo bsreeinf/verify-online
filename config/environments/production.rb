48cfd41e32e9a7de16d2795e3717444fdfb35cec
Sreenath B <bsreeinf@gmail.com>
2016-07-17 17:21:37 +0530

    updated email pwd in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['MASTER_EMAIL'],
-    :password       => ENV['MASTER_EMAIL_PASSWORD'],
+    :password       => ENV['MASTER_PWD'],
     :domain         => 'herokuapp.com',
     :enable_starttls_auto => true    
   }
