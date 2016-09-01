22d2f27b64ae87720ac85ea42be16f93bf2d5eef
Sreenath B <bsreeinf@gmail.com>
2016-09-01 12:58:49 +0530

    minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
-  host = 'verifyonline.herokuapp.com'
+  host = 'verifyonline.co.in'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
